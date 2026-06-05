55c258d7e03d196148db699ae4bec1ae86e14f00
kkovmat <druzokmatvej@gmail.com>
2026-06-05 15:44:56 +0300

    delete function

--- main.c
+++ main.c
@@ -1,247 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
-struct Row {
-    char* data;
-};
-
-
-struct Array {
-    struct Row* rows;
-    int rowsCount;
-    int currentRow;
-};
-
-
-struct Row* CreateRows(int rowsCount) {
-    struct Row* rows = malloc(rowsCount * sizeof(struct Row));
-    if (rows == NULL) {
-        printf("MEMORY ERROR");
-        return NULL;
-    }
-    for (int i = 0; i < rowsCount; i++)
-    {
-        rows[i].data = NULL;
-    }
-    return rows;
-}
-
-
-struct Array* CreateArray(int rowsCount) {
-    struct Array *array = malloc(sizeof(struct Array));
-    if (array == NULL) {
-        printf("MEMORY ERROR");
-        return NULL;
-    }
-    array->rows = CreateRows(rowsCount);
-    array->rowsCount = rowsCount;
-    array->currentRow = 0;
-    return array;
-}
-
-
-void AddToEnd(struct Array* array, char* text) {
-    int endRow = array->currentRow;
-    if (endRow >= array->rowsCount) {
-        int newRowsCount = array->rowsCount * 2;
-        struct Row* temp = realloc(array->rows, newRowsCount * sizeof(struct Row));
-        if (temp == NULL) {
-            printf("MEMORY ERROR");
-            return;
-        }
-        array->rows = temp;
-        for (int i = array->rowsCount; i < newRowsCount; i++) {
-            array->rows[i].data = NULL;
-        }
-        array->rowsCount = newRowsCount;
-    }
-    if (array->rows[endRow].data == NULL) {
-        array->rows[endRow].data = strdup(text);
-        return;
-    }
-    if (strlen(array->rows[endRow].data) == 0) {
-        strcpy(array->rows[endRow].data, text);
-        return;
-    }
-    int newLength = strlen(array->rows[endRow].data) + strlen(text) + 1;
-    char* temp = realloc(array->rows[endRow].data, newLength * sizeof(char));
-    if (temp == NULL) {
-        printf("MEMORY ERROR");
-        return;
-    }
-    array->rows[endRow].data = temp;
-    strcat(array->rows[endRow].data, text);
-}
-
-
-void AddNewLine(struct Array* array) {
-    AddToEnd(array, "\n");
-    array->currentRow += 1;
-    
-}
-
-
-_Bool Insert(struct Array* array, short line, short index, char* text) {
-   
-    if (array->rows[line].data == NULL || strlen(array->rows[line].data) == 0)  {
-        return 0;
-    }
-    if (index > strlen(array->rows[line].data)) {
-        return 0;
-    }
-    int newLength = strlen(array->rows[line].data) + strlen(text) + 1;
-    char* afterIndex = malloc((strlen(array->rows[line].data + index) + 1) * sizeof(char));
-    strcpy(afterIndex, array->rows[line].data + index);
-    char* temp = realloc(array->rows[line].data, newLength * sizeof(char));
-    if (temp == NULL) {
-        printf("MEMORY ERROR");
-        return 0;
-    }
-    array->rows[line].data = temp;
-    array->rows[line].data[index] = '\0';
-    strcat(array->rows[line].data, text);
-    strcat(array->rows[line].data, afterIndex);
-    
-    free(afterIndex);
-    return 1;
-}
-
-
-int Search(struct Array* array, char* text, int** indexes) {
-    
-    int lastIndex = 0;
-
-    for (int i = 0; i <= array->currentRow; i++) {
-        
-        
-        if (i == array->currentRow && (array->rows[array->currentRow].data == NULL || strlen(array->rows[array->currentRow].data) == 0)) {
-            break;
-        }
-
-        int textLen = strlen(text);
-        int rowLen = strlen(array->rows[i].data);
-        
-        if (array->rows[i].data == NULL || rowLen == 0) {
-            continue;
-        }
-
-        int lenCounter = 0;
-        
-        for (int j = 0; j < rowLen; j++) {
-            
-            if (array->rows[i].data[j] == '\0') {
-                break;
-            }
-
-            if (array->rows[i].data[j] == text[lenCounter]) {
-                lenCounter ++;
-            }
-            
-            else {
-                j -= lenCounter;
-                lenCounter = 0;
-            }
-            
-            if (lenCounter == textLen) {
-
-                int* temp = realloc(*indexes, (lastIndex + 2) * sizeof(int));
-                if (temp == NULL) {
-                    printf("MEMORY ERROR");
-                    return lastIndex;
-                }
-                
-                *indexes = temp;                
-                
-                (*indexes)[lastIndex] = i;
-                (*indexes)[lastIndex + 1] = j - (strlen(text) - 1);
-
-                lastIndex += 2;
-
-                j -= lenCounter - 1;
-                lenCounter = 0;
-                
-            }
-        }
-    }
-    return lastIndex;
-}
-
-
-void PrintFoundIndexes(int* indexes, int lastIndex) {
-    
-    for (int i = 0; i < lastIndex; i += 2) {
-        printf("(%d, %d) ", indexes[i], indexes[i + 1]);
-    }
-}
-
-
-void Print(struct Array* array) {
-    for (int i = 0; i < array->rowsCount; i++) {
-        if (array->rows[i].data == NULL) {
-            if (i >= array->currentRow) {
-                break;
-            }
-        }
-        else {
-            printf("%s", array->rows[i].data);
-        }
-        
-    }
-}
-
-
-void Free(struct Array* array) {
-    for (int i = 0; i < array->rowsCount; i++) {
-        free(array->rows[i].data);
-    }
-    free(array->rows);
-    free(array);
-}
-
-
-void SaveToFile(struct Array* array, char* fileName) {
-    FILE* file;
-    file = fopen(fileName, "w");
-    if (file != NULL) {
-        for (int i = 0; i < array->rowsCount; i++) {
-            if (array->rows[i].data != NULL) {
-                fputs(array->rows[i].data, file);
-            }
-        }
-        fclose(file);
-    }
-}
-
-
-_Bool LoadFromFile(struct Array** array, char* fileName) {
-    FILE* file;
-    char row[100];
-    file = fopen(fileName, "r");
-    if (file == NULL) {
-        printf("Error opening file.");
-        return 0;
-    }
-    else {
-        *array = CreateArray(10);
-        int i = 0;
-        while (fgets(row, sizeof(row), file) != NULL) {
-            AddToEnd(*array, row);
-            (*array)->currentRow++;
-            i++;
-        }
-        fclose(file);
-    }
-    return 1;
-}
-
-
-void ClearConsole() {
-    // \e[1J — очищає екран від курсора вгору
-    // \e[H  — повертає курсор у лівий верхній кут (0,0)
-    printf("\e[1J\e[H");
-}
+#include <stdbool.h>
+#include "text_editor.h"
 
 
 int main() {
@@ -253,7 +14,7 @@ int main() {
     while (1) {
 
         printf("\nSupported commands.");
-        printf("\n1. Append to end.\n2. New line.\n3. Save to file.\n4. Load from file.\n5. Print to console.\n6. Insert.\n7. Search.\n8. Exit.\n");
+        printf("\n1. Append to end.\n2. New line.\n3. Save to file.\n4. Load from file.\n5. Print to console.\n6. Insert.\n7. Search.\n8. Delete.\n9. Exit.\n");
 
         printf("\nChoose the command: ");
         scanf("%d", &command);
@@ -369,6 +130,24 @@ int main() {
             free(textPosition);
         }
         else if (command == 8) {
+            ClearConsole();
+            printf("Choose line, index and number of symbols: ");
+            short line;
+            short index;
+            int symbols;
+            if (scanf("%d %d %d", &line, &index, &symbols) != 3) {
+                printf("Invalid input.");
+                while(getchar() != '\n');
+            }
+            else {
+                while(getchar() != '\n');
+                if (!Delete(textArray, line, index, symbols)) {
+                    printf("Invalid line or index.");
+                }
+            }
+
+        }
+        else if (command == 9) {
             ClearConsole();
             break;
         }
---
+++ text_editor.h
@@ -0,0 +1,21 @@
+#ifndef TEXT_EDITOR_H
+#define TEXT_EDITOR_H
+#include <stdbool.h>
+
+struct Row;
+struct Array;
+struct Row* CreateRows(int rowsCount);
+struct Array* CreateArray(int rowsCount);
+void AddToEnd(struct Array* array, char* text);
+void AddNewLine(struct Array* array);
+bool Insert(struct Array* array, short line, short index, char* text);
+int Search(struct Array* array, char* text, int** indexes);
+void PrintFoundIndexes(int* indexes, int lastIndex);
+void Print(struct Array* array);
+bool Delete(struct Array* array, short line, short index, int symbols );
+void Free(struct Array* array);
+void SaveToFile(struct Array* array, char* fileName);
+bool LoadFromFile(struct Array** array, char* fileName);
+void ClearConsole();
+
+#endif
